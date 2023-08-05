894f1c556a37647b8a5dbae3f6ac7867b6a5ed81
Dmytro Stanchiev <git@old4ever.simplelogin.com>
2023-08-05 16:47:54 +0300

    Progress of chapter 99. Filter Items

--- script.js
+++ script.js
@@ -58,6 +58,21 @@ const clearItems = (e) => {
     checkUI();
 };
 
+const filterItems = (e) => {
+    const items = itemList.querySelectorAll("li");
+    const text = e.target.value.toLowerCase();
+
+    items.forEach((item) => {
+        const itemName = item.firstChild.textContent.toLowerCase();
+
+        if (itemName.indexOf(text) !== -1) {
+            item.style.display = "flex";
+        } else {
+            item.style.display = "none";
+        }
+    });
+};
+
 const checkUI = () => {
     const items = itemList.querySelectorAll("li");
 
@@ -73,5 +88,6 @@ const checkUI = () => {
 itemForm.addEventListener("submit", addItem);
 itemList.addEventListener("click", removeItem);
 clearBtn.addEventListener("click", clearItems);
+itemFilter.addEventListener("input", filterItems);
 
 checkUI();
