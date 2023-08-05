2f50d884a3f552fd86fcbe657ff6022064060d47
Dmytro Stanchiev <git@old4ever.simplelogin.com>
2023-08-05 16:30:12 +0300

    Progress of chapter 98. Clear UI State

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const itemForm = document.querySelector("#item-form");
 const itemInput = document.querySelector("#item-input");
 const itemList = document.querySelector("#item-list");
 const clearBtn = document.querySelector("#clear");
+const itemFilter = document.querySelector("#filter");
 
 const addItem = (e) => {
     e.preventDefault();
@@ -18,7 +19,10 @@ const addItem = (e) => {
 
     const button = createButton("remove-item btn-link text-red");
     li.appendChild(button);
+
     itemList.appendChild(li);
+
+    checkUI();
 
     itemInput.value = "";
 };
@@ -39,7 +43,11 @@ const createIcon = (classes) => {
 
 const removeItem = (e) => {
     if (e.target.parentElement.classList.contains("remove-item")) {
-        e.target.parentElement.parentElement.remove();
+        if (confirm("Are you sure?")) {
+            e.target.parentElement.parentElement.remove();
+
+            checkUI();
+        }
     }
 };
 
@@ -47,8 +55,23 @@ const clearItems = (e) => {
     while (itemList.firstChild) {
         itemList.removeChild(itemList.firstChild);
     }
+    checkUI();
+};
+
+const checkUI = () => {
+    const items = itemList.querySelectorAll("li");
+
+    if (items.length === 0) {
+        clearBtn.style.display = "none";
+        itemFilter.style.display = "none";
+    } else {
+        clearBtn.style.display = "block";
+        itemFilter.style.display = "block";
+    }
 };
 
 itemForm.addEventListener("submit", addItem);
 itemList.addEventListener("click", removeItem);
 clearBtn.addEventListener("click", clearItems);
+
+checkUI();
